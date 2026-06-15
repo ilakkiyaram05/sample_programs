a5b6dfaa52507dca150863d97d2b93498d123902
ilakkiyaram05 <ilakkiyaram05@gmail.com>
2026-06-16 00:37:08 +0530

    Implement merge function for binary trees

---
+++ Dp3.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def merge(t1, t2):
+
+    if t1 is None:
+        return t2
+
+    if t2 is None:
+        return t1
+
+    t1.value = t1.value + t2.value
+
+    t1.left = merge(t1.left, t2.left)
+
+    t1.right = merge(t1.right, t2.right)
+
+    return t1

ee8a7329839534515974c9d74a321968e8208176
ilakkiyaram05 <ilakkiyaram05@gmail.com>
2026-06-16 14:18:41 +0530

    Refactor frog jump solution to simplify logic

--- frog1.py
+++ frog1.py
@@ -1,32 +1,17 @@
-import sys
+n = int(input())
+# This reads the entire line of heights and converts them into a list of integers
+h = list(map(int, input().split()))
 
-def solve():
-    # Read N
-    n = int(sys.stdin.readline())
-    # Read heights
-    h = list(map(int, sys.stdin.readline().split()))
-   
-    # dp[i] will store the minimum cost to reach stone i
-    dp = [0] * n
-   
-    # Base Case: Stone 1 (index 0) is 0
-    dp[0] = 0
-   
-    # Base Case: Stone 2 (index 1)
-    if n > 1:
-        dp[1] = abs(h[1] - h[0])
-       
-    # Fill the table for the rest of the stones
-    for i in range(2, n):
-        # Option 1: Jump from the previous stone
-        opt1 = dp[i-1] + abs(h[i] - h[i-1])
-        # Option 2: Jump from two stones back
-        opt2 = dp[i-2] + abs(h[i] - h[i-2])
-       
-        # Take the minimum of the two
-        dp[i] = min(opt1, opt2)
-       
-    # The answer is the cost to reach the last stone
-    print(dp[n-1])
+dp = [0] * n
 
-solve()
+# Base cases
+dp[0] = 0
+if n > 1:
+    dp[1] = abs(h[1] - h[0])
+
+for i in range(2, n):
+    c1 = abs(h[i-1] - h[i])
+    c2 = abs(h[i-2] - h[i])
+    dp[i] = min(dp[i-1] + c1, dp[i-2] + c2)
+
+print(dp[n-1])
